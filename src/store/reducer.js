1c022cb37b0fce86bc61b3b52db23eed3400e933
emmeiwhite <emmeiwhite@gmail.com>
2018-10-04 15:58:51 +0530

    Added redux dependencies using yarn

---
+++ src/store/reducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    a:1,
+    b:1
+}
+
+// Let's create our reducer which is nothing but a function
+
+const reducer = (state=initialState,action)=>{
+    // Don't mutate the state
+    const newState = {...state};
+
+    return newState;
+}
