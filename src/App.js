751afa5d592c66562dd7f1ec3839b90b2ada19a8
emmeiwhite <emmeiwhite@gmail.com>
2018-10-04 16:40:16 +0530

    Slow and steady wins the race

--- src/App.js
+++ src/App.js
@@ -10,13 +10,13 @@ class App extends Component {
         <h2>BE YOURSELF, VALUE YOURSELF, CODE YOURSELF</h2>
         <div className="col-sm-12">
               <div className="leafFalls">
-                <h4> A: </h4>
-                <button className="btn btn-danger">People Change</button>
+                <h4> A: {this.props.a}</h4>
+                <button className="btn btn-danger" onClick={this.props.action1}>People Change</button>
               </div>
 
               <div className="leafFalls">
-                <h4> B: </h4>
-                <button className="btn btn-warning">State Change</button>
+                <h4> B: {this.props.b}</h4>
+                <button className="btn btn-warning" onClick={this.props.action2}>State Change</button>
               </div>
         </div>
       </div>
@@ -33,7 +33,7 @@ const mSTP = (state)=>{
 
 const mDTP = (dispatch)=>{
   return {
-    action1:()=>dispatch({type:'ACTION_1'}),
+    action1:()=>dispatch({type:'ACTION_1'}), // here the action is heading towards the reducer
     action2:()=>dispatch({type:'ACTION_2'}),
   }
 }
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -7,9 +7,26 @@ const initialState = {
 
 const reducer = (state=initialState,action)=>{
     // Don't mutate the state
-    const newState = {...state};
+    // const newState = {...state};
 
-    return newState;
+    switch(action.type){
+        case 'ACTION_1':
+        return{
+            ...state, // First Copy the state then update the newState
+            a:state.a + state.b
+        }
+
+        case 'ACTION_2':
+        return{
+            ...state,
+            b:state.a + state.b
+        }
+
+        default:
+        return state;
+            
+    }
+
 }
 
 export default reducer;
